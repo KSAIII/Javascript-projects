77dd93268bc3a61d621d9111f9a646d85465ea35
SAI <86056964+KSAIII@users.noreply.github.com>
2023-09-30 23:59:40 +0530

    Add files via upload
    **Welcome to Image Search App**
    
    Discover the world of visual exploration with our cutting-edge Image Search App. Whether you're a professional photographer, a creative artist, or simply someone who loves beautiful visuals, our platform is designed to help you find the perfect images with ease.
    
    **What We Offer:**
    
    1. **Effortless Image Discovery:** Our intuitive search interface allows you to find images that match your criteria quickly. With a vast database of high-quality pictures, you'll never run out of inspiration.
    
    2. **Advanced Filters:** Fine-tune your search results using a wide range of filters, including keywords, colors, resolution, and more. Say goodbye to endless scrolling and hello to precision in your search.
    
    3. **Personalized Collections:** Create your own image collections to keep your favorite visuals organized. You can also follow other users and explore their curated collections for fresh ideas.
    
    4. **AI-Powered Recommendations:** Let our intelligent algorithms suggest images that align with your preferences, making your search experience even more seamless.
    
    5. **Licensing Options:** Whether you need images for personal use, commercial projects, or educational purposes, we offer flexible licensing options to suit your needs.
    
    6. **Upload and Share:** Showcase your own photography talents by uploading your images to our platform. Share your work with the world and gain recognition as a photographer or artist.
    
    7. **Community Engagement:** Engage with a vibrant community of visual enthusiasts. Share your insights, exchange tips, and connect with like-minded individuals who share your passion for imagery.
    
    **Why Choose Image Search App:**
    
    - **Quality Assurance:** We curate our image library to ensure you have access to high-quality visuals that meet your standards.
    
    - **User-Friendly Experience:** Our user interface is designed for simplicity and ease of navigation, so you can focus on what you love – finding beautiful images.
    
    - **Constant Improvement:** We're committed to regular updates and improvements based on user feedback, ensuring our platform evolves to meet your needs.
    
    - **Privacy and Security:** Your data and privacy are our top priorities. Rest assured that your information is protected.
    
    Join the Image Search App community today and embark on a visual journey like no other. Whether you're looking for inspiration, professional resources, or a platform to share your own work, we're here to make your image search experience enjoyable and efficient.
    
    Start exploring the world of images with us. Your next masterpiece could be just a click away!
    
    ---
    
    Feel free to customize and adapt this description to best reflect the unique features and goals of your Image Search App website project.

---
+++ Image search using javascript/index.js
@@ -0,0 +1,55 @@
+const accesskey = "BDQp_yjsmWSpthOPXcNb4qEd-8uqA9RBhDvvx5P22ws";
+
+const formE1 = document.querySelector("form");
+const inputE1 = document.getElementById("search-input");
+const searchresult = document.querySelector(".search-results");
+const showmore = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+    inputData = inputE1.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accesskey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const results = data.results;
+    if(page === 1)
+    {
+        searchresult.innerHTML="";
+    }
+    results.map((result)=>{
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement('img');
+        image.src=result.urls.small ;
+        image.alt = result.alt_description ;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html ;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchresult.appendChild(imageWrapper);
+    });
+    page++;
+    if(page>1)
+    {
+        showmore.style.display = "block";
+    }
+
+}
+formE1.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    page = 1;
+    searchImages();
+});
+
+showmore.addEventListener("click",()=>{
+    
+   
+    searchImages();
+});
+
+
